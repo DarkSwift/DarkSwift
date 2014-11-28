19c55f3bf30aa2d1885380092104a7f20b161b28
DarkSwift <endo2014@india.com>
2014-11-28 17:55:31 +0300

    initial message

---
+++ README.md
@@ -0,0 +1,2 @@
+DarkSwift 
+
---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define DarkSwift-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "DarkSwiftURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
